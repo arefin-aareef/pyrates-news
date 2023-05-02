019485f3e5ece86df901399423e53057adaba82b
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-02 19:31:28 +0600

    icons added on right nav

--- client/src/pages/Shared/RightNav/RightNav.jsx
+++ client/src/pages/Shared/RightNav/RightNav.jsx
@@ -1,13 +1,26 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>Login With</h2>
-      <Button variant="outline-primary">Login with Google</Button>{" "}
-      <Button variant="outline-secondary">Login with Github</Button>{" "}
+      <h4>Login With</h4>
+      <Button className="mb-2" variant="outline-primary">
+        <FaGoogle /> Login with Google
+      </Button>
+      <Button variant="outline-secondary">
+        <FaGithub /> Login with Github
+      </Button>
+      <div>
+        <h4>Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item> <FaFacebook /> Facebook </ListGroup.Item>
+          <ListGroup.Item> <FaTwitter /> Twitter </ListGroup.Item>
+          <ListGroup.Item> <FaInstagram /> Instagram </ListGroup.Item>
+        </ListGroup>
+      </div>
     </div>
   );
 };
