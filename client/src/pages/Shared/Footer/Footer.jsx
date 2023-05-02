4472c73c04579c000eed22d1353d058b7edb9574
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-02 18:27:40 +0600

    created layout

---
+++ client/src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center'><small>Copyright | Arefin</small></p>
+        </div>
+    );
+};
+
+export default Footer;
