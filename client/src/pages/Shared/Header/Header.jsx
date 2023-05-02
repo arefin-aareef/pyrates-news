293ec18e29c9db299ba98817cab5e9a8bcbf304a
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-02 19:20:33 +0600

    react icons installed

--- client/src/pages/Shared/Header/Header.jsx
+++ client/src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import logo from "../../../assets/logo.png";
-import moment from 'moment';
+import moment from "moment";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <div className="text-center">
         <img src={logo} alt="" />
         <p className="text-secondary">
@@ -13,7 +15,32 @@ const Header = () => {
         </p>
         <p>{moment().format("dddd, MMMM D, YYYY")}</p>
       </div>
-    </div>
+      <div className="d-flex">
+        <Button variant="danger">Latest</Button>{" "}
+        <Marquee speed={60} className="text-danger">
+          I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Career</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+              <Button variant="primary">Login</Button>{' '}
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
   );
 };
 
--- client/src/pages/Shared/RightNav/RightNav.jsx
+++ client/src/pages/Shared/RightNav/RightNav.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>Right Nav</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Login With</h2>
+      <Button variant="outline-primary">Login with Google</Button>{" "}
+      <Button variant="outline-secondary">Login with Github</Button>{" "}
+    </div>
+  );
 };
 
 export default RightNav;
