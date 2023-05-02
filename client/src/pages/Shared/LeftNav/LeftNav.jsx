1f9fa7b3f49dd1734145dcdb786480a0873113e9
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-02 23:05:26 +0600

    element added on left nav

--- client/src/pages/Shared/LeftNav/LeftNav.jsx
+++ client/src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,23 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/categories')
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
-    }, [])
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-
-
-    return (
-        <div>
-            <h2>Left Nav</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h4>All Category</h4>
+      <div className="ps-4">
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link to={`/category${category.id}`} className="text-decoration-none">{category.name}</Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
