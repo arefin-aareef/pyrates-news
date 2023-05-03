3e125fb9a5d95ea969e895fb500a27f0eeb8eb81
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-03 16:38:13 +0600

    createUser added

--- client/src/pages/Login/Register/Register.jsx
+++ client/src/pages/Login/Register/Register.jsx
@@ -1,13 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
 
 const Register = () => {
+    const { createUser } = useContext(AuthContext);
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const photo = form.photo.value;
+        const password = form.password.value;
+
+        console.log(name, email, photo, password);
+
+        createUser(email, password)
+        .then(result => {
+            const createdUser = result.user;
+            console.log(createdUser);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
     return (
         <Container className="mx-auto w-25">
       <h3>Please Register</h3>
-      <Form>
+      <Form onSubmit={handleRegister}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
           <Form.Control type="name" name="name" placeholder="Enter name" required />
