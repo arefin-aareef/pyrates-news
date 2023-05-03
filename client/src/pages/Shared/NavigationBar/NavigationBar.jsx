263157a41ec40dbbd8c232bdce2b6b738d3c84b9
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-03 15:05:19 +0600

    login layout added

---
+++ client/src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext } from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
+
+const NavigationBar = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Container>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Link to="/">Home</Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Career</Nav.Link>
+            </Nav>
+            <Nav>
+              {user && (
+                <Nav.Link href="#deets">
+                  <FaUserCircle style={{ fontSize: "2rem" }} />
+                </Nav.Link>
+              )}
+              {user ? (
+                <Button variant="primary">Log out</Button>
+              ) : (
+                <Link to="/login">
+                  <Button variant="primary">Login</Button>
+                </Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
+  );
+};
+
+export default NavigationBar;
