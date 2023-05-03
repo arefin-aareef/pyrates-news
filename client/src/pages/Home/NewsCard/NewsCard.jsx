070c9a419a3e9addc9faf0201055a07ecc78dae9
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-03 20:28:59 +0600

    few changes

--- client/src/pages/Home/NewsCard/NewsCard.jsx
+++ client/src/pages/Home/NewsCard/NewsCard.jsx
@@ -5,7 +5,8 @@ import React from "react";
 import { Button, Card, Image } from "react-bootstrap";
 import { FaRegBookmark, FaRegEye, FaShareAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const NewsCard = ({ news }) => {
   const { _id, title, details, image_url, author, total_view, rating } = news;
@@ -13,33 +14,44 @@ const NewsCard = ({ news }) => {
   return (
     <Card className="mb-4">
       <Card.Header className="d-flex align-items-center">
-      <Image style={{height:'40px'}} src={author?.img} roundedCircle />
-      <div className="ps-2 flex-grow-1">
-        <p className="mb-0">{author?.name}</p>
-        <p><small> {moment(author.published_date).format('yyyy-MM-D')}</small></p>
-      </div>
-      <div>
-        <FaRegBookmark></FaRegBookmark>
-        <FaShareAlt></FaShareAlt>
-      </div>
+        <Image style={{ height: "40px" }} src={author?.img} roundedCircle />
+        <div className="ps-2 flex-grow-1">
+          <p className="mb-0">{author?.name}</p>
+          <p>
+            <small> {moment(author.published_date).format("yyyy-MM-D")}</small>
+          </p>
+        </div>
+        <div>
+          <FaRegBookmark></FaRegBookmark>
+          <FaShareAlt></FaShareAlt>
+        </div>
       </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Img variant="top" src={image_url} />
         <Card.Text>
-            {details.length < 250 ? <>{details}</> : 
-            <>{details.slice(0, 250)}... <Link to={`/news/${_id}`}>Read More</Link> </>
-            }
+          {details.length < 250 ? (
+            <>{details}</>
+          ) : (
+            <>
+              {details.slice(0, 250)}...{" "}
+              <Link to={`/news/${_id}`}>Read More</Link>{" "}
+            </>
+          )}
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
       <Card.Footer className="text-muted d-flex">
-        <div className="flex-grow-1">
-            
-            <span>{rating?.number}</span>
+        <div className="flex-grow-1 d-flex align-items-center">
+          <Rating
+            style={{ maxWidth: 100 }}
+            value={Math.round(rating?.number || 0)}
+            readOnly
+          />
+          <span className="ms-2">{rating?.number}</span>
         </div>
         <div>
-            <FaRegEye></FaRegEye> {total_view}
+          <FaRegEye></FaRegEye> {total_view}
         </div>
       </Card.Footer>
     </Card>
--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -40,7 +40,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+                loader: ({params}) => fetch(`https://pyrates-news-arefin-aareef.vercel.app/categories/${params.id}`)
             }
         ]
     },
@@ -51,7 +51,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <PrivateRoutes> <News></News> </PrivateRoutes>,
-                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch(`https://pyrates-news-arefin-aareef.vercel.app/news/${params.id}`)
             }
         ]
     }
